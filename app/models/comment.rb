ddbc6ce4e9d6e3d95550d7b4f2b0f091f9526c88
Amir Khan <ak2022dev@gmail.com>
2023-11-23 18:23:07 +0000

    Refactor app/models/comment.rb to include Visible and so remove duplicate code

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,5 @@
 class Comment < ApplicationRecord
+  include Visible
+  
   belongs_to :article
-
-  VALID_STATUSES = [ 'public', 'private', 'archived' ]
-
-  def archived?
-    status == 'archived'
-  end
 end
